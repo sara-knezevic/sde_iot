9529cb8eb6d17d63c7ef6db6940480361e551bf5
Sara <lost.func@gmail.com>
2020-12-15 00:10:56 +0100

    PC to Arduino commands

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -15,17 +15,23 @@
 #define LIGHT_SENSOR_ROOM1 A2
 #define LIGHT_SENSOR_ROOM2 A3
 
-static const uint8_t DEVICE_ADDRESS = 0x01;
-static const uint8_t PC_ID = 0x02;
-
-byte messageArray[16];
+byte sensorArray[16];
+uint8_t device, room, state;
+int pickedDevice, pickedRoom, pickedState;
 
 int lightSensorRoom1, lightSensorRoom2;
 unsigned long previousMillis = 0;
-bool freshAirIsOn = false;
+
+int lightsRoom1 = 0;
+int ventillationOnRoom1 = 0; // ventillation state
+bool freshAirIsOnRoom1 = true;
+
+int lightsRoom2 = 0;
+int ventillationOnRoom2 = 0; // ventillation state
+bool freshAirIsOnRoom2 = true;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   
   pinMode(RELAY_LIGHT_ROOM1, OUTPUT);
   pinMode(RELAY_LIGHT_ROOM2, OUTPUT);
@@ -40,23 +46,39 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(EN_ROOM1, 255); // == PWM
-  analogWrite(EN_ROOM2, 255); // == PWM
-
   unsigned long currentMillis = millis();
-
-  // Each minute
-  if (currentMillis % 60000 == 0) {
-    // Ventillation system
-    if (freshAirIsOn) {
-      airSuction(DC_PIN1_ROOM1, DC_PIN2_ROOM1, false);
-      freshAir(DC_PIN1_ROOM1, DC_PIN2_ROOM1, true);
-    } else {
-      freshAir(DC_PIN1_ROOM1, DC_PIN2_ROOM1, false);
-      airSuction(DC_PIN1_ROOM1, DC_PIN2_ROOM1, true);
+  
+  if (Serial.available() > 0) {
+    getCommands();
+  }
+  
+  // Each 5 seconds
+  if (currentMillis % 5000 == 0) {
+    // Ventillation system Room 1
+    // Autonomous loop
+    if (ventillationOnRoom1) {
+      if (freshAirIsOnRoom1) {
+        airSuction(DC_PIN1_ROOM1, DC_PIN2_ROOM1, false);
+        freshAir(DC_PIN1_ROOM1, DC_PIN2_ROOM1, true);
+      } else {
+        freshAir(DC_PIN1_ROOM1, DC_PIN2_ROOM1, false);
+        airSuction(DC_PIN1_ROOM1, DC_PIN2_ROOM1, true);
+      }
+      freshAirIsOnRoom1 = !freshAirIsOnRoom1;
     }
 
-    freshAirIsOn = !freshAirIsOn;
+    // Ventillation system Room 2
+    // Autonomous loop
+    if (ventillationOnRoom2) {
+      if (freshAirIsOnRoom2) {
+        airSuction(DC_PIN1_ROOM2, DC_PIN2_ROOM2, false);
+        freshAir(DC_PIN1_ROOM2, DC_PIN2_ROOM2, true);
+      } else {
+        freshAir(DC_PIN1_ROOM2, DC_PIN2_ROOM2, false);
+        airSuction(DC_PIN1_ROOM2, DC_PIN2_ROOM2, true);
+      }
+      freshAirIsOnRoom2 = !freshAirIsOnRoom2;
+    }
 
     // Sensors
     float measuredTemperature1 = voltageToTemperature(analogRead(TEMP_ROOM1));
@@ -64,8 +86,26 @@ void loop() {
     float measuredIlluminance1 = convertIlluminance(analogRead(LIGHT_SENSOR_ROOM1));
     float measuredIlluminance2 = convertIlluminance(analogRead(LIGHT_SENSOR_ROOM2));
 
-    sendSensorData(measuredTemperature1, measuredTemperature2, measuredIlluminance1, measuredIlluminance2);
+//    sendSensorData(measuredTemperature1, measuredIlluminance1, measuredTemperature2, measuredIlluminance2);
   }
+}
+
+void getCommands() {
+   byte rcv[3];
+   Serial.readBytes(rcv, 3);
+   device = rcv[0];
+   room = rcv[1];
+   state = rcv[2];
+
+   pickedDevice = (int)device;
+   pickedRoom = (int)room;
+   pickedState = (int)state;
+
+   if (pickedDevice == 1) { // ventillation
+      ventillationState(pickedRoom, pickedState);
+   } else if (pickedDevice == 2) { // lights
+      lightState(pickedRoom, pickedState);
+   }
 }
 
 void sendSensorData(float T1, float L1, float T2, float L2) {
@@ -77,28 +117,64 @@ void sendSensorData(float T1, float L1, float T2, float L2) {
   addrT2 = (uint32_t *)&T2;
   addrL2 = (uint32_t *)&L2;
   
-  messageArray[0] = (uint8_t)(*addrT1 & 0xFF);
-  messageArray[1] = (uint8_t)(*addrT1 >> 8) & 0xFF;
-  messageArray[2] = (uint8_t)(*addrT1 >> 16) & 0xFF;
-  messageArray[3] = (uint8_t)(*addrT1 >> 24) & 0xFF;
+  sensorArray[0] = (uint8_t)(*addrT1 & 0xFF);
+  sensorArray[1] = (uint8_t)(*addrT1 >> 8) & 0xFF;
+  sensorArray[2] = (uint8_t)(*addrT1 >> 16) & 0xFF;
+  sensorArray[3] = (uint8_t)(*addrT1 >> 24) & 0xFF;
   
-  messageArray[4] = (uint8_t)(*addrL1 & 0xFF);
-  messageArray[5] = (uint8_t)(*addrL1 >> 8) & 0xFF;
-  messageArray[6] = (uint8_t)(*addrL1 >> 16) & 0xFF;
-  messageArray[7] = (uint8_t)(*addrL1 >> 24) & 0xFF;
+  sensorArray[4] = (uint8_t)(*addrL1 & 0xFF);
+  sensorArray[5] = (uint8_t)(*addrL1 >> 8) & 0xFF;
+  sensorArray[6] = (uint8_t)(*addrL1 >> 16) & 0xFF;
+  sensorArray[7] = (uint8_t)(*addrL1 >> 24) & 0xFF;
   
-  messageArray[8] = (uint8_t)(*addrT2 & 0xFF);
-  messageArray[9] = (uint8_t)(*addrT2 >> 8) & 0xFF;
-  messageArray[10] = (uint8_t)(*addrT2 >> 16) & 0xFF;
-  messageArray[11] = (uint8_t)(*addrT2 >> 24) & 0xFF;
+  sensorArray[8] = (uint8_t)(*addrT2 & 0xFF);
+  sensorArray[9] = (uint8_t)(*addrT2 >> 8) & 0xFF;
+  sensorArray[10] = (uint8_t)(*addrT2 >> 16) & 0xFF;
+  sensorArray[11] = (uint8_t)(*addrT2 >> 24) & 0xFF;
   
-  messageArray[12] = (uint8_t)(*addrL2 & 0xFF);
-  messageArray[13] = (uint8_t)(*addrL2 >> 8) & 0xFF;
-  messageArray[14] = (uint8_t)(*addrL2 >> 16) & 0xFF;
-  messageArray[15] = (uint8_t)(*addrL2 >> 24) & 0xFF;
+  sensorArray[12] = (uint8_t)(*addrL2 & 0xFF);
+  sensorArray[13] = (uint8_t)(*addrL2 >> 8) & 0xFF;
+  sensorArray[14] = (uint8_t)(*addrL2 >> 16) & 0xFF;
+  sensorArray[15] = (uint8_t)(*addrL2 >> 24) & 0xFF;
 
-  Serial.write(messageArray, 16);
+  Serial.write(sensorArray, 16);
   delay(1000);
+}
+
+void ventillationState(int room, int state) {
+  if (room == 1) {
+      ventillationOnRoom1 = state;
+      if (state == 1) {
+        analogWrite(EN_ROOM1, 255);
+      } else {
+        analogWrite(EN_ROOM1, 0);
+      }
+   } else if (room == 2) {
+      ventillationOnRoom2 = state;
+      if (state == 1) {
+        analogWrite(EN_ROOM2, 255);
+      } else {
+        analogWrite(EN_ROOM2, 0);
+      } 
+   }
+}
+
+void lightState(int room, int state) {
+  if (room == 1) {
+      lightsRoom1 = state;
+      if (state == 1) {
+        digitalWrite(RELAY_LIGHT_ROOM1, HIGH);
+      } else {
+        digitalWrite(RELAY_LIGHT_ROOM1, LOW);
+      }
+   } else if (room == 2) {
+      lightsRoom2 = state;
+      if (state == 1) {
+        digitalWrite(RELAY_LIGHT_ROOM2, HIGH);
+      } else {
+        digitalWrite(RELAY_LIGHT_ROOM2, LOW);
+      }
+   }
 }
 
 void freshAir(int pin1, int pin2, bool on) {
@@ -121,15 +197,15 @@ void airSuction(int pin1, int pin2, bool on) {
   }
 }
 
-float voltageToTemperature(float voltage) {
-  float milivoltage = (voltage/1023.0) * 5000;
-  float temperature = milivoltage / 10;
+float voltageToTemperature(int voltage) {
+  float milivoltage = (voltage/1023.) * 5000.;
+  float temperature = milivoltage / 10.;
   return temperature;
 }
 
-float convertIlluminance(int resistance) {
-  float cal = (250.0/(0.0048828125*resistance)) - 50.0;
-  float difference = 33.0 - cal;
-  float lux = int(33.0 + difference);
-  return lux;
+float convertIlluminance(int lightVoltage) {
+  float resistorVoltage = (float)lightVoltage / (1023.0 * 5.0);
+  float ldrVoltage = 5.0 - resistorVoltage;
+  float ldrResistance = ldrVoltage / resistorVoltage * 10.0;
+  return ldrResistance;
 }
